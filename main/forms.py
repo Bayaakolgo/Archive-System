a2bf76a334cb7036e926c0ff0d143a222bf126d2
Bayaakolgo <mohammedabass75@gmail.com>
2023-04-18 16:12:35 +0000

    Made the app more colorful and user friendly

--- main/forms.py
+++ main/forms.py
@@ -10,15 +10,53 @@ class UploadForm(ModelForm):
         model = Upload
         fields = ['name','file']
  
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Username",
+                "class": "form-control"
+            }
+        ))
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password",
+                "class": "form-control"
+            }
+        ))
 
 class NewUserForm(UserCreationForm):
-    
+
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Username",
+                "class": "form-control"
+            }
+        ))
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "placeholder": "Email",
+                "class": "form-control"
+            }
+        ))
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password",
+                "class": "form-control"
+            }
+        ))
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password check",
+                "class": "form-control"
+            }
+        ))
+
     class Meta:
         model = User
-        fields = ('username', 'password1', 'password2')
-    
-    def save(self, commit=True):
-        user = super(NewUserForm, self).save(commit=False)
-        if commit:
-            user.save()
-        return user
+        fields = ('username', 'email', 'password1', 'password2')
--- main/views.py
+++ main/views.py
@@ -1,30 +1,28 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
-from .forms import NewUserForm
+from .forms import NewUserForm,UploadForm, LoginForm
 from .models import Upload
-from .forms import UploadForm
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
-@login_required
+@login_required(login_url="/login/")
 def delete_request(request, pk):
     document = get_object_or_404(Upload, pk=pk)
     document.delete()
     return redirect('dashboard')
 
-@login_required
+@login_required(login_url="/login/")
 def download_request(request, pk):
     document = get_object_or_404(Upload, pk=pk)
     response = HttpResponse(document.file, content_type='application/pdf')
     response['Content-Disposition'] = f'attachment; filename="{document.file.name}"'
     return response
 
-@login_required
+@login_required(login_url="/login/")
 def upload_request(request):
     if request.method == 'POST':
         form = UploadForm(request.POST, request.FILES)
@@ -39,11 +37,11 @@ def upload_request(request):
         "form": form
     })
 
-@login_required
+@login_required(login_url="/login/")
 def dashboard_request(request):
     uploads = Upload.objects.all()
     context = {'uploads':uploads}
-    return render(request, 'main/dashboard.html', context=context)
+    return render(request, 'main/index.html', context=context)
 
 def logout_request(request):
     logout(request)
@@ -54,21 +52,30 @@ def register_request(request):
     if request.method == "POST":
         form = NewUserForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
+            form.save()
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=raw_password)
+
             messages.success(request, 'Registration successfull.')
             return redirect('login')
-        messages.error(request, 'Unsuccessful registration. Invalid information.')
-    form = NewUserForm()
-    return render(request=request, template_name='main/register.html', context={'register_form':form})
+        else:
+            messages.error(request, 'Unsuccessful registration. Invalid information.')
+    else:
+        form = NewUserForm()
+
+    return render(request=request, template_name='main/register.html', context={'form':form})
 
 def login_request(request):
+    form = LoginForm(request.POST or None)
+
     if request.method == "POST":
-        form = AuthenticationForm(request, data=request.POST)
+
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
+
             if user is not None:
                 login(request, user)
                 messages.info(request, f"You are now logged in as {username}.")
@@ -77,5 +84,6 @@ def login_request(request):
                 messages.error(request, 'Invalid username or password.')
         else:
             messages.error(request, 'Invalid username or password.')
-    form = AuthenticationForm()
-    return render(request=request, template_name='main/login.html', context={'login_form':form})
+    form = LoginForm()
+
+    return render(request=request, template_name='main/login.html', context={'form':form})
